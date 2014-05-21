acb6510de58b560b4002ec2721018b5cc93742cb
Chanisco <chaniscotromp@chaniscos-mbp.media.local>
2014-05-21 11:34:29 +0200

    example level complete

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,33 +2,51 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
-	
+	bool standingOnPlane;
+
 	// Update is called once per frame
 	void Update () {
-		if(Global.turnWorld == 0){
-			if(Input.GetKey(KeyCode.A)){
-				transform.Translate(-5 * Time.deltaTime	,0,0);
-			}else if(Input.GetKey(KeyCode.D)){
-				transform.Translate(5  * Time.deltaTime,0,0);
-			}	
-		}else if(Global.turnWorld == 1){
-			if(Input.GetKey(KeyCode.A)){
-				transform.Translate(0,-5 * Time.deltaTime,0);
-			}else if(Input.GetKey(KeyCode.D)){
-				transform.Translate(0,5  * Time.deltaTime,0);
-			}
-		}else if(Global.turnWorld == 2){
-			if(Input.GetKey(KeyCode.A)){
-				transform.Translate(5 * Time.deltaTime	,0,0);
-			}else if(Input.GetKey(KeyCode.D)){
-				transform.Translate(-5  * Time.deltaTime,0,0);
-			}
-		}else if(Global.turnWorld == 3){
-			if(Input.GetKey(KeyCode.A)){
-				transform.Translate(0,5 * Time.deltaTime,0);
-			}else if(Input.GetKey(KeyCode.D)){
-				transform.Translate(0,-5  * Time.deltaTime,0);
+		if(standingOnPlane){
+			if(Global.turnWorld == 0){
+				if(Input.GetKey(KeyCode.A)){
+					transform.Translate(-10 * Time.deltaTime	,0,0);
+				}else if(Input.GetKey(KeyCode.D)){
+					transform.Translate(10  * Time.deltaTime,0,0);
+				}	
+			}else if(Global.turnWorld == 1){
+				if(Input.GetKey(KeyCode.A)){
+					transform.Translate(0,-10 * Time.deltaTime,0);
+				}else if(Input.GetKey(KeyCode.D)){
+					transform.Translate(0,10  * Time.deltaTime,0);
+				}
+			}else if(Global.turnWorld == 2){
+				if(Input.GetKey(KeyCode.A)){
+					transform.Translate(10 * Time.deltaTime	,0,0);
+				}else if(Input.GetKey(KeyCode.D)){
+					transform.Translate(-10  * Time.deltaTime,0,0);
+				}
+			}else if(Global.turnWorld == 3){
+				if(Input.GetKey(KeyCode.A)){
+					transform.Translate(0,10 * Time.deltaTime,0);
+				}else if(Input.GetKey(KeyCode.D)){
+					transform.Translate(0,-10  * Time.deltaTime,0);
+				}
 			}
 		}
 	}
+
+	void OnCollisionEnter(Collision other) {
+		if(other.collider.tag == "Platform"){
+			Debug.Log("ik sta erop");
+			standingOnPlane = true;
+		}
+	}
+	void OnCollisionExit(Collision other){
+		if(other.collider.tag == "Platform"){
+			standingOnPlane = false;
+		}
+	}
+
+
+
 }
