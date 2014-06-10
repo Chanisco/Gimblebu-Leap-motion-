98cff84b57615fdc7aae17bbaa89df9e9287db13
Chanisco <chaniscotromp@chaniscos-mbp.media.local>
2014-06-10 14:12:03 +0200

    Decimaal Check void problem

--- Assets/Scripts/LeapMechanics/Resize.cs
+++ Assets/Scripts/LeapMechanics/Resize.cs
@@ -5,16 +5,19 @@ using Leap;
 
 public class Resize : MonoBehaviour {
 	GameObject parent;
-
+	Decimale decimaal;
 	void Awake(){
+		decimaal = new Decimale();
 		parent = transform.parent.gameObject;
 	}
 	public void fingerCheck(Controller ctrl){
 		Frame frame = ctrl.Frame();
 		Finger leftFingr = frame.Fingers.Leftmost;
 		Finger rightFingr = frame.Fingers.Rightmost;
+		float LeftFinger = decimaal.TwoDecimal(leftFingr.Direction.x);
+		float rightFinger = decimaal.TwoDecimal(rightFingr.Direction.x);
 
-		Debug.Log ("Linker Finger = " + leftFingr.Direction.x +  "Rechter finger = " + rightFingr.Direction.x);
+		Debug.Log ("Linker Finger = " + LeftFinger +  "Rechter finger = " + rightFinger);
 
 
 	}
--- Assets/Scripts/Math/Decimale.cs
+++ Assets/Scripts/Math/Decimale.cs
@@ -5,6 +5,6 @@ public class Decimale {
 
 	public void TwoDecimal(float Number){
 		Number = Mathf.Round(Number * 100) / 100;
-		return Number
+		return Number;
 	}
 }
